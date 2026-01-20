78ee7bffe8ee6ed678052ca0015936741a187257
Ming <xumingwei_0@163.com>
2026-01-20 23:07:27 +0800

    取消通知

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,10 @@
     <div id="app" class="min-h-screen">
         <router-view />
         <!-- <FloatingDonation /> -->
-        <GlobalNoticeModal />
     </div>
 </template>
 
 <script setup>
 // App组件
 // import FloatingDonation from './components/FloatingDonation.vue'
-import GlobalNoticeModal from './components/GlobalNoticeModal.vue'
 </script>
